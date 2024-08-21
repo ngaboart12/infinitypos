14a42df95f21875db2e2bd523f6a72f1ad193baf
sevelinCA <ngabosevelin@gmail.com>
2024-08-21 09:45:17 +0200

    Add link to features

--- app/components/landing/Navbar.tsx
+++ app/components/landing/Navbar.tsx
@@ -7,12 +7,12 @@ const Navbar = () => {
         <div className='font-[800]'>LOGO</div>
         <div className='hidden flex-row items-center lg:flex lg:gap-[40px]'>
             <a href="/" className='text-[14px] text-[#01A653] font-[600]'>Home</a>
-            <a href="#" className='text-[14px] text-black font-[600]'>Features</a>
+            <a href="/features" className='text-[14px] text-black font-[600]'>Features</a>
             <a href="/pricing" className='text-[14px] text-black font-[600]'>Pricing</a>
             <a href="#" className='text-[14px] text-black font-[600]'>Contact Us </a>
         </div>
         <div>
-         <Button href='/auth' background='#01A653' label='Register' />
+         <Button href='/auth' background='#01A653' label='Sign In' />
         </div>
     </div>
   )
